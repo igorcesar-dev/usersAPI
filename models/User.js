9763d5f96d74a355d0a4af14bdf234a75ed26198
igorcesar-dev <igorcesar.dev@gmail.com>
2023-03-15 18:19:13 -0300

    criando_busca_de_usuario

--- models/User.js
+++ models/User.js
@@ -2,6 +2,35 @@ var knex = require("../database/connection");
 var bcrypt = require("bcrypt");
 
 class User {
+  async findAll() {
+    try {
+      var result = await knex
+        .select(["id", "email", "name", "role"])
+        .table("users");
+      return result;
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
+  }
+
+  async findById(id) {
+    try {
+      var result = await knex
+        .select(["id", "email", "name", "role"])
+        .where({ id: id })
+        .table("users");
+      if (result.length > 0) {
+        return result[0];
+      } else {
+        return undefined;
+      }
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
+  }
+
   async new(email, password, name) {
     try {
       var hash = await bcrypt.hash(password, 5);
@@ -13,18 +42,18 @@ class User {
     }
   }
 
-  async findEmail(email){
-    try{
-        var result = await knex.select("*").from("users").where({email:email});
-        
-        if(result.length > 0){
-            return true;
-        }else{
-            return false;
-        }
-    }catch(err){
-        console.log(err);
+  async findEmail(email) {
+    try {
+      var result = await knex.select("*").from("users").where({ email: email });
+
+      if (result.length > 0) {
+        return true;
+      } else {
         return false;
+      }
+    } catch (err) {
+      console.log(err);
+      return false;
     }
   }
 }
